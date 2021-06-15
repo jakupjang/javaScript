ec01cfad8e6784d4b2e6528a9e0cc23a137586ec
jakupjang <11e11@naver.com>
2021-06-15 09:34:22 +0900

    자바 스크립트 파일

---
+++ js/event.js
@@ -0,0 +1,10 @@
+//상세 설명 보기 클릭 - 텍스트 보기
+function showText(){
+    var text = document.getElementById("desc");
+    text.style.display = "block"; //수직으로 보이기
+}
+//상세 설명 닫기 클릭 - 텍스트 숨기기
+function hideText(){
+    var text = document.getElementById("desc");
+    text.style.display = "none"; //텍스트 숨기기
+}
---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+        // 오른쪽 이미지 클릭하면 이미지 넘기기
+        var num = 1;
+        function nextGallery(){
+            num += 1;
+            if(num > 7){
+                num = 1;
+            }
+            
+            document.getElementById("gallery").src = "../images/img" + num + ".jpg";
+        }
+        // 왼쪽 이미지를 클릭하면 이미지 넘기기
+        function prevGallery(){
+            num -= 1;
+            if(num < 1){
+                num = 7;
+            }
+            document.getElementById("gallery").src = "../images/img" + num + ".jpg";
+        }
