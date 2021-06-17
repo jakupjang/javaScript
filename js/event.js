98241a87d69980c036401fe029437f0fc806c0cb
jakupjang <11e11@naver.com>
2021-06-17 10:04:52 +0900

    자바스크립트 소스파일

--- js/event.js
+++ js/event.js
@@ -1,10 +1,20 @@
 //상세 설명 보기 클릭 - 텍스트 보기
 function showText(){
-    var text = document.getElementById("desc");
-    text.style.display = "block"; //수직으로 보이기
+    //var text = document.getElementById("desc");
+    //text.style.display = "block"; //수직으로 보이기
+
+    document.getElementById("desc").style.display = "block";
+
+    //var button = document.getElementById("open");
+    //button.style.display = "none";
+
+    document.getElementById("open").style.display = "none";
 }
 //상세 설명 닫기 클릭 - 텍스트 숨기기
 function hideText(){
     var text = document.getElementById("desc");
     text.style.display = "none"; //텍스트 숨기기
+
+    var button = document.getElementById("open");
+    button.style.display = "block";
 }
